22b1132b1a6f3f71607ecc616bab0f1b37a65426
Yugala1234 <108147535+Yugala1234@users.noreply.github.com>
2025-08-15 18:13:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
-def greet(name):
-    return f"Hello, {name}!"
+import requests
+
+def get_status(url):
+    response = requests.get(url)
+    return response.status_code
+
+if __name__ == "__main__":
+    print(get_status("https://example.com"))
